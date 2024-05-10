ecad072c5d3aac54dd4ab7104ab78abb4eb5c825
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-05-10 20:12:04 +0530

    create button

--- src/main/java/org/example/supermarketmanagementsystem/HelloController1.java
+++ src/main/java/org/example/supermarketmanagementsystem/HelloController1.java
@@ -1,4 +1,23 @@
 package org.example.supermarketmanagementsystem;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HelloController1 {
+    @FXML
+    private Button goBack;
+
+    @FXML
+    void goBackWelcomePage(MouseEvent event) throws IOException {
+        Stage stage = (Stage)goBack.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        stage.setTitle("Supermarket");
+        stage.setScene(new Scene(fxmlLoader.load()));
+    }
 }
--- src/main/java/org/example/supermarketmanagementsystem/HomePage.java
+++ src/main/java/org/example/supermarketmanagementsystem/HomePage.java
@@ -1,4 +1,23 @@
 package org.example.supermarketmanagementsystem;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePage {
+    @FXML
+    private Button logout;
+
+    @FXML
+    void onLogout(MouseEvent event) throws IOException {
+        Stage stage = (Stage)logout.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        stage.setTitle("Supermarket");
+        stage.setScene(new Scene(fxmlLoader.load()));
+    }
 }
